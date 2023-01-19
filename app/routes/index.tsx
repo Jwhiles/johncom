1b209e7de898d163423dca4269267bd17225603c
John Whiles <johnwhiles@outlook.com>
2023-01-19 07:28:42 +0100

    add most popular posts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from "@remix-run/react";
 
-
 export default function Index() {
   return (
     <div className="p-4 leading-tight max-w-2xl mx-auto">
@@ -16,6 +15,20 @@ export default function Index() {
           <Link to="/sport_quotes">My favourite sports quotes</Link>
         </li>
       </ul>
+      <div>
+        <h2>Most Popular Posts</h2>
+        <ul>
+          <li>
+            <Link to="/posts/work">Maybe you should do less 'work'</Link>
+          </li>
+          <li>
+            <Link to="/posts/shaving">Shaving, or 'the world is out to get you'</Link>
+          </li>
+          <li>
+            <Link to="/posts/music-production-lessons">Six things I believe about making music</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

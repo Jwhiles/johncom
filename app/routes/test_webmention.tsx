c704ab44b5771469d1ff9e758ad858836285cd1a
John Whiles <johnwhiles@outlook.com>
2023-12-27 13:12:11 +0000

    Create a test webmention

---
+++ app/routes/test_webmention.tsx
@@ -0,0 +1,11 @@
+export default function TestWebmention() {
+  return (
+    <>
+      <p>
+        in reply to:{" "}
+        <a href="https://johnwhiles.com/posts/vimming-pains">@jwhiles</a>
+      </p>
+      <p>Testing webmentions</p>
+    </>
+  );
+}

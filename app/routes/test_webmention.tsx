ad1aa9c4096c397672511d8ccc5c51612ddb961d
John Whiles <johnwhiles@outlook.com>
2023-12-27 14:09:10 +0000

    update test webmention

--- app/routes/test_webmention.tsx
+++ app/routes/test_webmention.tsx
@@ -1,11 +1,38 @@
 export default function TestWebmention() {
   return (
     <>
-      <p>
-        in reply to:{" "}
-        <a href="https://johnwhiles.com/posts/vimming-pains">@jwhiles</a>
-      </p>
-      <p>Testing webmentions</p>
+      <div className="h-entry">
+        <div className="u-author h-card">
+          <img
+            src="https://images.ctfassets.net/wc253zohgsra/6ldaNVODgtTNBymgloaY3Z/c36d9234283255a4802cb949b8c0dfad/john_logo.png"
+            alt="John Whiles"
+            className="u-photo"
+            width="40"
+          />
+          <a href="https://johnwhles.com/" className="u-url p-name">
+            John Whiles
+          </a>
+        </div>
+        <p>
+          in reply to:{" "}
+          <a
+            className="u-in-reply-to"
+            href="https://johnwhiles.com/posts/vimming-pains"
+          >
+            @jwhiles
+          </a>
+        </p>
+        <p className="e-content">
+          Testing my webmention endpoint. This is a reply to a post on my site.
+        </p>
+        <p>
+          <a href="http://johnwhiles.com/test_webmention" className="u-url">
+            <time className="dt-published" dateTime="2023-12-27:15:00-0700">
+              Dec 27, 2023
+            </time>
+          </a>
+        </p>
+      </div>
     </>
   );
 }

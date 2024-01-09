d5932f36b4c4a0c4bba1f741d84e8ab7da27ceb5
John Whiles <johnwhiles@outlook.com>
2024-01-09 14:56:22 +0000

    add CV link to homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,14 @@
 import { HeadersFunction, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import dayjs from "dayjs";
 
-import { Note } from "~/components/Note";
 import { getListOfEntries } from "~/contentful.server";
-import { prisma } from "~/db.server";
-import { renderToHtml } from "~/features/markdown/index.server";
 import { formatDate } from "~/utils/formatDate";
 
 export const loader = async () => {
   const latestPost = (await getListOfEntries()).items[0];
 
-  const notes = (
-    await prisma.note.findMany({ orderBy: { createdAt: "desc" }, take: 5 })
-  ).map((post) => {
-    return {
-      id: post.id,
-      content: renderToHtml(post.content),
-      // Dates... Do I want to add dayjs to this projects?
-      createdAt: dayjs(post.createdAt).format("ddd, MMM D, YYYY h:mmA Z"),
-      inReplyToUrl: post.inReplyToUrl,
-      inReplyToAuthor: post.inReplyToAuthor,
-      inReplyToTitle: post.inReplyToTitle,
-    };
-  });
-
   return json(
-    { latestPost, notes },
+    { latestPost },
     { headers: { "cache-control": "max-age=300, s-maxage=3600" } },
   );
 };
@@ -47,8 +29,8 @@ export default function Index() {
           </Link>
         </li>
         <li>
-          <Link className="text-2xl" to="/contact">
-            Talk to me?
+          <Link className="text-2xl" to="/cv">
+            My CV
           </Link>
         </li>
         <li>
@@ -67,11 +49,6 @@ export default function Index() {
         <div className="text-xs dark:text-slate-300">
           {formatDate(latestPost.fields.date)}
         </div>
-
-        <div className="text-base font-bold text-slate-300 mt-8 mb-1">
-          Stream of consciousness
-        </div>
-        <Notes />
       </div>
       <img
         alt="John’s logo"
@@ -84,19 +61,3 @@ export default function Index() {
     </div>
   );
 }
-
-const Notes = () => {
-  const { notes } = useLoaderData<typeof loader>();
-  return (
-    <ol>
-      {notes.map((post) => (
-        <li key={post.id}>
-          <Note {...post} />
-        </li>
-      ))}
-      <Link className="text-xs" to={`/notes`}>
-        See more
-      </Link>
-    </ol>
-  );
-};

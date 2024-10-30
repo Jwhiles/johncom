af0d71caf2da6b664b06f5aebe3ea0d81442825e
John Whiles <johnwhiles@outlook.com>
2024-10-30 20:53:03 +0000

    add tags to db, and migration script

--- mocks/index.js
+++ mocks/index.js
@@ -3,20 +3,20 @@ const { setupServer } = require("msw/node");
 const { entry } = require("./contentfulReturn");
 
 const handlers = [
-  http.get(
-    "https://cdn.contentful.com/spaces/:spaceid/entries",
-    async () => {
-      return HttpResponse.json(entry);
-    }
-  ),
-  http.post("https://api.eu.mailgun.net/v3/:domain/messages", async (req) => {
-    const text = await req.request.text();
-    console.log("🔶 mocked email text:", text);
+  // http.get(
+  //   "https://cdn.contentful.com/spaces/:spaceid/entries",
+  //   async () => {
+  //     return HttpResponse.json(entry);
+  //   }
+  // ),
+  // http.post("https://api.eu.mailgun.net/v3/:domain/messages", async (req) => {
+  //   const text = await req.request.text();
+  //   console.log("🔶 mocked email text:", text);
 
-    const randomId = "20210321210543.1.E01B8B612C44B41B";
-    const id = `<${randomId}>@${req.params.domain}`;
-    return HttpResponse.json({ id, message: "Queued. Thank you." });
-  }),
+  //   const randomId = "20210321210543.1.E01B8B612C44B41B";
+  //   const id = `<${randomId}>@${req.params.domain}`;
+  //   return HttpResponse.json({ id, message: "Queued. Thank you." });
+  // }),
 ];
 
 const server = setupServer(...handlers);
---
+++ prisma/migrations/20241029184737_add_tags/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_PostToTag" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_PostToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Post" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_PostToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_slug_key" ON "Tag"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostToTag_AB_unique" ON "_PostToTag"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostToTag_B_index" ON "_PostToTag"("B");
